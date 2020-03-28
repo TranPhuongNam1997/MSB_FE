ff32636a7d92027431d6403a77d91025bb1b3b95
chungnt <chungnguyentrong@admicro.vn>
2020-03-28 11:49:54 +0700

    update

--- MSB_Project/libs/js/script.js
+++ MSB_Project/libs/js/script.js
@@ -1,11 +1,9 @@
 $( document ).ready(function() {
 
-    var getwidth = $('.data-filed .datepicker-msb-none').width();
-    $('.xdsoft_datetimepicker.xdsoft_noselect.xdsoft_').load(function(){
-
-        $(this).css({"width": getwidth +'!important' });
+    $('.datepicker-msb-none').focus(function () {
+        var getwidth = $('.data-filed .datepicker-msb-none').width();
+        $('.xdsoft_datetimepicker.xdsoft_noselect.xdsoft_').css({"width": getwidth + 38});
     });
-    console.log(getwidth + 38);
 
     //select
     $('select').click(function () {
@@ -27,6 +25,29 @@ $( document ).ready(function() {
     overlay.click(function () {
        $(".view-menu-mb").trigger('click');
     });
+
+    //close file name-uploadfile
+    $(".close-file").click(function () {
+        $(this).parent(".file-return").remove();
+    });
+
+    login_pp();
+});
+
+
+$(window).resize(function () {
+    login_pp();
+});
+$(document).click(function (event) {
+    // Check if clicked outside target
+    if (!($(event.target).closest(".user-block").length)) {
+        // Hide target
+        $(".info-user-wrap").hide();
+
+    }
+});
+// fc menumb
+function login_pp() {
     if (window.innerWidth < 992) {
         $(".info-user-wrap").hide();
         $(".user-block").click(function () {
@@ -46,8 +67,4 @@ $( document ).ready(function() {
             }
         });
     }
-    //close file name-uploadfile
-    $(".close-file").click(function () {
-        $(this).parent(".file-return").remove();
-    })
-});
+}
